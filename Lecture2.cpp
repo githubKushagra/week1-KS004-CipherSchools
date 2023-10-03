15b278673baf65100a8ce3ddcff96b5a7b951238
Kushagra Varshney <118505260+githubKushagra@users.noreply.github.com>
2023-10-03 17:37:21 +0530

    Add files via upload

---
+++ Lecture2.cpp
@@ -0,0 +1,11 @@
+// This is a comment
+#include <iostream>      // This is a header file.
+
+using namespace std;    // std makes sure that cout is a part of "iostream".
+
+int main(){
+
+    cout << "Hello, World";
+
+    return 0;
+}
