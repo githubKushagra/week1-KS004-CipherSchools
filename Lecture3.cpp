0ac2b68ae57dc26badec8899c70c741022a77099
Kushagra Varshney <118505260+githubKushagra@users.noreply.github.com>
2023-10-03 18:03:46 +0530

    Add files via upload

---
+++ Lecture3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>       // for 'string' data type, we have to include this header file
+
+using namespace std;
+
+int main(){
+
+    // various data types - int,float,char,double,long,string,etc.
+    // syntax for declaring a data type - data_type + one_space + variable_name;
+    // variables like - a,abc,any_name,etc.
+
+    int number;           // int is a data type and number is a variable.
+    string name;
+}
