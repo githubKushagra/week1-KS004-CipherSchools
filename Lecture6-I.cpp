dc4835a1efaa669c407ceeaf3478717baf7b9599
Kushagra Varshney <118505260+githubKushagra@users.noreply.github.com>
2023-10-07 00:32:46 +0530

    Add files via upload

---
+++ Lecture6-I.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    // for loop syntax -  for(initialization , condition , updation) { code ;}
+
+    // example for for-loop (table of 2).F
+    for (int i = 0; i < 10; i++)
+    {
+        cout << "2 x " << i + 1 << " = " << 2 * (i + 1) << endl;
+    }
+
+    return 0;
+}
